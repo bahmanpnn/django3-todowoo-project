c3728c96b72f7a4bd184528a97ed1b274f5d4a7f
Bahman Pournazari <bahmanpn@gmail.com>
2023-04-02 20:31:41 +0430

    18-create a list create todo apiview and update delete detail apiview

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todos/completed', views.TodoCompletedListView.as_view(), name="todo-completed-list")
+    path('todos/', views.TodoListCreateView.as_view(), name="todo-list"),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroyView.as_view(), name="todo-detail-page"),
+    path('todos/completed', views.TodoCompletedListView.as_view(), name="todo-completed-list"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,28 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from todo.models import Todo
-
 from .serializers import TodoCompletedListSerializer
+
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    serializer_class = TodoCompletedListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=True)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoCompletedListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
 
 
 class TodoCompletedListView(generics.ListAPIView):
